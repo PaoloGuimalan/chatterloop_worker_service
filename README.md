35de513d1de707585afa32037d045bea4646a4b2
Paolo <pauloportes.guimalan187@gmail.com>
2026-08-15 07:59:04 +0800

    initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+## Worker Service for Chatterloop
+
+## Getting Started
+
+Installation
+
+- Download go installation and run
+
+Project Creation
+
+- run go mod init worker_service (for empty project)
+
+Build (Debug)
+
+- run go build main.go
---
+++ cmd/worker/main.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+var waitgroup = sync.WaitGroup{}
+func main(){
+	var intNum int = 10;
+	fmt.Println(intNum)
+
+	waitgroup.Add(1)
+	go printMe("Hello World") 
+	// run concurrent using go (goroutines)
+	// can use var m = sync.Mutex{} / sync.RWMutex{} for multiaccess data
+	// locking data on edit like actual database, prevent race conditions
+
+	//m.Lock() / m.RLock()
+	// execution block
+	// m.Unlock() / m.RUnlock
+	waitgroup.Wait()
+
+	var newuser User
+
+	newuser.name = "John"
+	var output string = newuser.BuildUser("johndoe@gmail.com")
+
+	fmt.Println(output)
+
+	var anotheruser User
+	fmt.Println(anotheruser, newuser)
+
+	newuser.userID = "123456"
+
+	fmt.Println(newuser)
+}
+
+func printMe(value string){
+	fmt.Println(value)
+	fmt.Println(&value)
+	waitgroup.Done()
+}
+
+type User struct {
+	userID string
+	name string
+}
+
+func (usr User) BuildUser(email string) string {
+	return "Hello, World! I am " + usr.name + " with email: " + email
+}
